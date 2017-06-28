fe13b63d9509461e8ff1d3f693fe157ad50796a0
graeberlab <nbalanis@mednet.ucla.edu>
2017-06-27 20:44:41 -0700

    Update PLSR_from_file_and_predict_second_dataset.R

--- R/PLSR_from_file_and_predict_second_dataset.R
+++ R/PLSR_from_file_and_predict_second_dataset.R
@@ -18,7 +18,11 @@
 #' @param sample.names2 Vector of sample names in 2nd dataset, if needed
 #' @param sample.type2 Vector of sample types in 2nd dataset, if needed
 #' @param train_string string to insert in file name of predicted scores
-#' 
+#' @param saveplot whether to save the plot, default is F
+#' @param savename the name to save plot under
+#' @param savetype the type of plot to save,options are ".pdf" or ".png"
+#' @param w is width of plot to be saved
+#' @param h is height of plot to be saved
 #' @export
 #'
 
@@ -37,82 +41,82 @@
 # sample.names2 = info$sample
 # sample.type2 = info$type
 # train_string = "tofile1"
-PLSR_from_file_and_predict_second_dataset = function(file, file2, sample.names, sample.type, y.response, sample.names2=NULL, sample.type2=NULL, train_string,
-                                                     title = "PLSR", comp.x = "comp.1", comp.y = "comp.2",comps = 3, scale = F, labels = F){
+PLSR_from_file_and_predict_second_dataset <- function (file, file2, sample.names, sample.type, y.response, 
+          sample.names2 = NULL, sample.type2 = NULL, train_string, 
+          title = "PLSR", comp.x = "comp.1", comp.y = "comp.2", comps = 3, 
+           labels = F,saveplot=F,savename="default",
+          savetype = ".pdf", w = 8, h = 6, 
+          legendname = "default",scale=F) {
   require(mixOmics)
-  data = read.table(file, sep='\t',header=T,stringsAsFactors=FALSE, quote = "")
-  data = data[rowSums((data[, -1] == 0)) < ncol(data[-1]), ] #remove genes with no variance
-  data2 = read.table(file2, sep='\t',header=T,stringsAsFactors=FALSE, quote = "")
-  #data2 = data2[rowSums((data2[, -1] == 0)) < ncol(data2[-1]), ] #remove genes with no variance
-  
-  data = data[!duplicated(data[,1]), ]
-  data2 = data2[!duplicated(data2[,1]), ]
-  
-  common.genes = intersect_all(data[,1], data2[,1])
-  data = data[data[,1] %in% common.genes, ]
-  data2 = data2[data2[,1] %in% common.genes, ]
-  data = data[order(data[,1]), ]
-  data2 = data2[order(data2[,1]), ]
-  
-  rownames(data) = make.names(data[, 1], unique=TRUE)
+  data = read.table(file, sep = "\t", header = T, stringsAsFactors = FALSE, 
+                    quote = "")
+  data = data[rowSums((data[, -1] == 0)) < ncol(data[-1]), 
+              ]
+  data2 = read.table(file2, sep = "\t", header = T, stringsAsFactors = FALSE, 
+                     quote = "")
+  data = data[!duplicated(data[, 1]), ]
+  data2 = data2[!duplicated(data2[, 1]), ]
+  common.genes = intersect_all(data[, 1], data2[, 1])
+  data = data[data[, 1] %in% common.genes, ]
+  data2 = data2[data2[, 1] %in% common.genes, ]
+  data = data[order(data[, 1]), ]
+  data2 = data2[order(data2[, 1]), ]
+  rownames(data) = make.names(data[, 1], unique = TRUE)
   t.data = data.frame(t(data[, -1]))
-  y.response = (data.frame(y.response)[match(rownames(t.data), as.character(sample.names)), ])
+  y.response = (data.frame(y.response)[match(rownames(t.data), 
+                                             as.character(sample.names)), ])
   y.response = as.matrix(y.response)
-  
-  pls.fit = pls(X = t.data, Y = y.response, scale = scale, ncomp = comps) 
-  
+  pls.fit = pls(X = t.data, Y = y.response, scale = scale, 
+                ncomp = comps)
   x.variates = data.frame(pls.fit$variates$X)
-  
-  #plot the first
-  x.variates$type = sample.type[match(rownames(x.variates), sample.names)]
-  pc.pred = ggplot(data=x.variates, aes_string(x = comp.x, y = comp.y)) + geom_point(size = I(2), aes(color = factor(type))) +
-    theme(legend.position = "right", plot.title = element_text(size = 30), legend.text = element_text(size = 22),
-          legend.title = element_text(size = 20), axis.title = element_text(size = 30), legend.background = element_rect(),
-          axis.text.x = element_text(margin = margin(b = -2)), axis.text.y = element_text(margin = margin(l = -14))) +
-    labs(title = title) + theme_bw()+
-    if (labels == TRUE) {
-      geom_text(data = x.variates, mapping = aes(label =(rownames(x.variates))), check_overlap = TRUE, size = 3)
+  x.variates$type = sample.type[match(rownames(x.variates), 
+                                      sample.names)]
+  pc.pred = ggplot(data = x.variates, aes_string(x = comp.x, 
+                                                 y = comp.y)) + geom_point(size = I(2), aes(color = factor(type))) + 
+    theme(legend.position = "right", plot.title = element_text(size = 30), 
+          legend.text = element_text(size = 22), legend.title = element_text(size = 20), 
+          axis.title = element_text(size = 30), legend.background = element_rect(), 
+          axis.text.x = element_text(margin = margin(b = -2)), 
+          axis.text.y = element_text(margin = margin(l = -14))) + 
+    labs(title = title) + theme_bw() + if (labels == TRUE) {
+      geom_text(data = x.variates, mapping = aes(label = (rownames(x.variates))), 
+                check_overlap = TRUE, size = 2.5)
     }
   pc.pred
-  
-  #project second dataset
-  rownames(data2) = make.names(data2[, 1], unique=TRUE)
-  t.data2 = data.frame(t(data2[,-1])) 
-  
-  
+  rownames(data2) = make.names(data2[, 1], unique = TRUE)
+  t.data2 = data.frame(t(data2[, -1]))
   test.predict <- predict(pls.fit, t.data2)
   prediction <- as.data.frame(test.predict$variates)
   colnames(prediction) <- colnames(x.variates)[-ncol(x.variates)]
-  
-  write.table(cbind(Sample = rownames(prediction),(prediction)), paste0(gsub(".txt", "", file2),"_", train_string, "_PLSR_predicted.scores.txt"), sep = "\t", row.names = F, quote = F)
-  
-  
-  
-  
-  prediction$type = sample.type2[match(rownames(prediction), sample.names2)]
-  #plot the prediction
-  pc.pred <- ggplot(prediction, aes_string(x = comp.x, y = comp.y)) + geom_point(size = I(2), aes(color = factor(type))) +
-    theme(legend.position = "right", plot.title = element_text(size = 30), legend.text = element_text(size = 22),
-          legend.title = element_text(size = 20), axis.title = element_text(size = 30), legend.background = element_rect(),
-          axis.text.x = element_text(margin = margin(b = -2)), axis.text.y = element_text(margin = margin(l = -14))) +
-    guides(color=guide_legend(title="Type"))+
-    labs(title = title) + theme_bw() +
-    if (labels == TRUE) {
-      geom_text(data = prediction, mapping = aes(label =(rownames(prediction))), check_overlap = TRUE, size = 3)
+  write.table(cbind(Sample = rownames(prediction), (prediction)), 
+              paste0(gsub(".txt", "", file2), "_", train_string, "_PLSR_predicted.scores.txt"), 
+              sep = "\t", row.names = F, quote = F)
+  prediction$type = sample.type2[match(rownames(prediction), 
+                                       sample.names2)]
+  pc.pred <- ggplot(prediction, aes_string(x = comp.x, y = comp.y)) + 
+    geom_point(size = I(2), aes(color = factor(type))) + 
+    theme(legend.position = "right", plot.title = element_text(size = 30), 
+          legend.text = element_text(size = 22), legend.title = element_text(size = 20), 
+          axis.title = element_text(size = 30), legend.background = element_rect(), 
+          axis.text.x = element_text(margin = margin(b = -2)), 
+          axis.text.y = element_text(margin = margin(l = -14))) + 
+    guides(color = guide_legend(title = "Type")) + labs(title = title) + 
+    theme_bw() + if (labels == TRUE) {
+      geom_text(data = prediction, mapping = aes(label = (rownames(prediction))), 
+                check_overlap = TRUE, size = 2.3)
     }
   pc.pred
-  
-  pc.pred = pc.pred + geom_point(data=x.variates, aes_string(x = comp.x, y = comp.y)) + geom_point(size = I(2), aes(color = factor(type))) +
-    theme(legend.position = "right", plot.title = element_text(size = 30), legend.text = element_text(size = 22),
-          legend.title = element_text(size = 20), axis.title = element_text(size = 30), legend.background = element_rect(),
-          axis.text.x = element_text(margin = margin(b = -2)), axis.text.y = element_text(margin = margin(l = -14))) +
+  pc.pred = pc.pred + geom_point(data = x.variates, aes_string(x = comp.x, 
+                                                               y = comp.y)) + geom_point(size = I(1.3), aes(color = factor(type))) + 
+    theme(legend.position = "right", plot.title = element_text(size = 30), 
+          legend.text = element_text(size = 22), legend.title = element_text(size = 20), 
+          axis.title = element_text(size = 30), legend.background = element_rect(), 
+          axis.text.x = element_text(margin = margin(b = -2)), 
+          axis.text.y = element_text(margin = margin(l = -14))) + 
     labs(title = title) + theme_bw()
-  
+  if(saveplot==T){
+  ggsave(paste0(savename, "_", comp.x, "_vs_", comp.y, savetype), 
+         dpi = 300, plot = pc.pred, width = w, height = h)
+  }
   pc.pred
-  
-  
-  
 }
-
-
-
