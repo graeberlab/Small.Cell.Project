44188e0cfb6cd5215b22e8dfba13ae88a8bbeda8
graeberlab <nbalanis@mednet.ucla.edu>
2017-08-23 14:41:52 -0700

    Update plot_pca_projection_all.R
    added color palette
    added ability to have legend

--- R/plot_pca_projection_all.R
+++ R/plot_pca_projection_all.R
@@ -11,6 +11,7 @@
 #' @param title Title of the plot
 #' @param labels default=T
 #' @param PCx,PCy PCs to display
+#' @param colpalette allows you to use a manual color palette using vector of form c('red','blue"....etc)
 #' 
 # @importFrom ggplot2 ggplot aes aes_string element_rect element_text geom_point geom_text labs margin theme theme_bw
 #' 
@@ -18,27 +19,36 @@
 #'
 
 
-plot_pca_projection_all = function(file, rotated.file, info.name, info.type, info.name2, info.type2, title = "Projection", labels = F, PCx="PC1", PCy="PC2"){
+ plot_pca_projection_all<-function (file, rotated.file, info.name, info.type, info.name2, 
+          info.type2, title = "Projection", labels = F, PCx = "PC1", 
+          PCy = "PC2",colpalette=NULL,legendname="default") 
+{
   require(ggplot2)
   pc.scores = read.table(file, header = TRUE, row.names = 1)
   pc.scores.reduced = pc.scores[, 1:5]
-  pc.scores.reduced$type = info.type[match(rownames(pc.scores.reduced), info.name)]
-  
-  projected_data = read.table(rotated.file,header = T, row.names = 1)
-  projected_data.reduced = projected_data[,1:5]
-  projected_data.reduced$type = info.type2[match(rownames(projected_data.reduced), info.name2)]
-  
+  pc.scores.reduced$type = info.type[match(rownames(pc.scores.reduced), 
+                                           info.name)]
+  projected_data = read.table(rotated.file, header = T, row.names = 1)
+  projected_data.reduced = projected_data[, 1:5]
+  projected_data.reduced$type = info.type2[match(rownames(projected_data.reduced), 
+                                                 info.name2)]
   combined.data = rbind(pc.scores.reduced, projected_data.reduced)
-  colnames(combined.data)[6] <-"type" 
-  
-  pcx.y <- ggplot(combined.data, aes_string(x=PCx,y=PCy)) +geom_point(size = I(2), aes(color = factor(type))) +
-    theme(legend.position="right",plot.title=element_text(size=30),legend.text=element_text(size=22),
-          legend.title=element_text(size=20),axis.title=element_text(size=30),legend.background = element_rect(),
-          axis.text.x = element_text(margin = margin(b=-2)),axis.text.y = element_text(margin = margin(l=-14)))+
-    guides(color=guide_legend(title="Type"))+
-    labs(title = title)+
-    theme_bw()+
-    if(labels==TRUE){geom_text(mapping = aes(label = rownames(combined.data)), check_overlap = TRUE, size = 3)}
-  pcx.y
+  colnames(combined.data)[6] <- "type"
+  pcx.y <- ggplot(combined.data, aes_string(x = PCx, y = PCy)) + 
+    geom_point(size = I(2), aes(color = factor(type))) + 
+    theme(legend.position = "right", plot.title = element_text(size = 30), 
+          legend.text = element_text(size = 22), legend.title = element_text(size = 20), 
+          axis.title = element_text(size = 30), legend.background = element_rect(), 
+          axis.text.x = element_text(margin = margin(b = -2)), 
+          axis.text.y = element_text(margin = margin(l = -14))) + 
+    guides(color = guide_legend(title = "Type")) + labs(title = title) + 
+    theme_bw() 
+    if (!is.null(colpalette)) 
+      pcx.y = pcx.y + scale_color_manual(legendname, values = colpalette)
 
+    if (labels == TRUE) {
+      geom_text(mapping = aes(label = rownames(combined.data)), 
+                check_overlap = TRUE, size = 3)
+    }
+  pcx.y
 }
