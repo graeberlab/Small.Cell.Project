d2c01437e332f6bab8da011c560a30e65ab1f2d3
graeberlab <nbalanis@mednet.ucla.edu>
2017-08-24 16:12:48 -0700

    Update PLSR_from_file_and_predict_second_dataset.R
    add varimax option

--- R/PLSR_from_file_and_predict_second_dataset.R
+++ R/PLSR_from_file_and_predict_second_dataset.R
@@ -27,6 +27,8 @@
 #' @param plot_both if true plots both training and test set in color
 #' @param colpalette allows you to put in a color palette of form c("#F87660", "#39B600",....etc) to manually assign colors
 #' @param shape.palette allows you to put in a shape palette of form c(1, 3,....etc) to manually assign shapes
+#' @param varimax If T performs Varimax rotation, 
+#' @param varimax.comp # of varimax components, kind of hacky, keep this # the same as # of comps. Will fix later.
 #' @export
 #'
 
@@ -47,10 +49,11 @@
 # train_string = "tofile1"
 PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, sample.type, y.response, 
                                                      sample.names2 = NULL, sample.type2 = NULL, train_string, 
-                                                     title = "PLSR", comp.x = "comp.1", comp.y = "comp.2", comps = 3, 
+                                                     title = "PLSR", comp.x = "comp.1", comp.y = "comp.2", comps = 2, 
                                                      labels = F,saveplot=F,savename="default",
-                                                     savetype = ".pdf", w = 8, h = 6, 
-                                                     legendname = "default",scale=F,plot_both=F,colpalette=NULL,shape.palette=NULL) {
+                                                     savetype = ".pdf", w = 8, h = 6, legendname = "default",scale=F,
+                                                     plot_both=F,colpalette=NULL,shape.palette=NULL,ellipses=T,conf=0.9,
+                                                     varimax=F,varimax.comp=2) {
   require(mixOmics)
   data = read.table(file, sep = "\t", header = T, stringsAsFactors = FALSE, 
                     quote = "")
@@ -73,6 +76,30 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
   pls.fit = pls(X = t.data, Y = y.response, scale = scale, 
                 ncomp = comps)
   x.variates = data.frame(pls.fit$variates$X)
+  x.loadings = data.frame(pls.fit$loadings$X)
+  
+  if(varimax==T){
+    
+    rotation = varimax(as.matrix(x.loadings[, c(1:(varimax.comp))]), normalize = F) 
+    #rot.load = as.data.frame(matrix(unlist(rotation$loadings), nrow=nrow(x.loadings), byrow=T)) 
+    #rot.load = cbind(Loading = x.loadings[,1], rot.load)
+    
+    
+    scores <- scale(x.variates[,1:(varimax.comp)]) %*% rotation$rotmat
+    scores = as.data.frame(scores)
+    colnames(scores)=colnames(x.variates)
+    x.variates=scores
+    
+    #scores = cbind(Score = rownames(x.variates), scores)
+    #write.table(scores, paste0(gsub(".txt", "", file.scores), "_VARIMAX.txt"), row.names = F, sep = "\t", quote = F)
+    
+    
+  }
+  
+  
+  
+  
+  
   x.variates$type = sample.type[match(rownames(x.variates), 
                                       sample.names)]
   pc.pred = ggplot(data = x.variates, aes_string(x = comp.x, 
@@ -91,7 +118,16 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
   t.data2 = data.frame(t(data2[, -1]))
   test.predict <- predict(pls.fit, t.data2)
   prediction <- as.data.frame(test.predict$variates)
+  
   colnames(prediction) <- colnames(x.variates)[-ncol(x.variates)]
+  
+  
+  if(varimax==T){
+    predit <- scale(prediction[,1:(varimax.comp)]) %*% rotation$rotmat
+    colnames(predit)=colnames(prediction)
+    prediction=as.data.frame(predit)
+    
+  }
   write.table(cbind(Sample = rownames(prediction), (prediction)), 
               paste0(gsub(".txt", "", file2), "_", train_string, "_PLSR_predicted.scores.txt"), 
               sep = "\t", row.names = F, quote = F)
@@ -120,17 +156,19 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
             axis.text.x = element_text(margin = margin(b = -2)), 
             axis.text.y = element_text(margin = margin(l = -14))) + 
       labs(title = title) + theme_bw() +
-    if (labels == TRUE) {
-      geom_text(data = comb, mapping = aes(label = (rownames(comb))), 
-                check_overlap = TRUE, size = 2.5)
-    }
+      if (labels == TRUE) {
+        geom_text(data = comb, mapping = aes(label = (rownames(comb))), 
+                  check_overlap = TRUE, size = 2.5)
+      }
     if(!is.null(shape.palette)){
       pc.pred3<- pc.pred3 + scale_shape_manual(legendname,values=shape.palette)
     }
     if(!is.null(colpalette)){
       pc.pred3<- pc.pred3 + scale_color_manual(legendname,values=colpalette)
     }
-    
+    if(ellipses==T){
+      pc.pred3<- pc.pred3 + stat_ellipse(aes(color=factor(type)),level=conf)
+    }
     
     if(saveplot==T){
       ggsave(paste0(savename, "_", comp.x, "_vs_", comp.y, savetype), 
