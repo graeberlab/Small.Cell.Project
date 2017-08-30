332ca9fa9534fa9359ee13e86398b63be9e78f89
graeberlab <nbalanis@mednet.ucla.edu>
2017-08-29 23:33:24 -0700

    Update PLSR_from_file.R
    added varimax option. it only corrects scores output. the loadings are the old version. this is a bug. will update soon.

--- R/PLSR_from_file.R
+++ R/PLSR_from_file.R
@@ -11,43 +11,52 @@
 #' @param labels label the plot, default = F
 #' @param comp.x,comp.y comps to display
 #' @param title title of the plot
+#' @param varimax performs varimax 
 #' 
 #' @export
 #'
-
-PLSR_from_file = function(file, sample.names, sample.type, y.response, title = "PLSR",comps = 5, scale = F, comp.x = "comp.1", comp.y = "comp.2", labels = F){
+PLSR_from_file<-function (file, sample.names, sample.type, y.response, title = "PLSR", 
+          comps = 5, scale = F, comp.x = "comp.1", comp.y = "comp.2", 
+          labels = F,varimax=F,varimax.comp=5) {
   require(mixOmics)
   require(ggplot2)
-  data = read.table(file, sep='\t',header=T,stringsAsFactors=FALSE, quote = "")
-  data = data[rowSums((data[, -1] == 0)) < ncol(data[-1]), ] #remove genes with no variance
-  rownames(data) = make.names(data[, 1], unique=TRUE)
+  data = read.table(file, sep = "\t", header = T, stringsAsFactors = FALSE, 
+                    quote = "")
+  data = data[rowSums((data[, -1] == 0)) < ncol(data[-1]), 
+              ]
+  rownames(data) = make.names(data[, 1], unique = TRUE)
   t.data = data.frame(t(data[, -1]))
-  y.response = (data.frame(y.response)[match(rownames(t.data), sample.names), ])
+  y.response = (data.frame(y.response)[match(rownames(t.data), 
+                                             sample.names), ])
   y.response = as.matrix(y.response)
- 
-  pls.fit = pls(X = t.data, Y = y.response, scale = scale, ncomp = comps) 
+  pls.fit = pls(X = t.data, Y = y.response, scale = scale, 
+                ncomp = comps)
   print(pls.fit$explained_variance$X)
-  
   x.variates = data.frame(pls.fit$variates$X)
   x.loadings = data.frame(pls.fit$loadings$X)
+  if(varimax==T){
+  rotation = varimax(as.matrix(x.loadings[, c(1:(varimax.comp))]), 
+                     normalize = F)
+  scores <- as.matrix(x.variates[, 1:(varimax.comp)]) %*% 
+    rotation$rotmat
+  scores = as.data.frame(scores)
+  colnames(scores) = colnames(x.variates)
+  x.variates = scores
+}
+  
   x.exp_variance = data.frame(pls.fit$explained_variance$X)
-  variates.X = cbind(Score = rownames(pls.fit$variates$X), x.variates)
-  loadings.X = cbind(Loading = rownames(pls.fit$loadings$X), x.loadings)
-  rownames(x.exp_variance) = paste0("comp.",seq(1,nrow(x.exp_variance)))
-  
-  write.table(as.data.frame(variates.X), paste0(gsub(".txt", "", file), "_PLSR_Xscores.txt"), sep = "\t", row.names = F, quote = F)
-  write.table(as.data.frame(loadings.X), paste0(gsub(".txt", "", file), "_PLSR_Xloadings.txt"), sep = "\t", row.names = F, quote = F)
-  write.table(as.data.frame(x.exp_variance), paste0(gsub(".txt", "", file), "_PLSR_Xpve.txt"), sep = "\t", row.names = T, quote = F)
-  
-  #plot it 
-  # variates.X$type = sample.type[match(rownames(variates.X), sample.names)]
-  # pcx.y <- ggplot(variates.X, aes_string(x = comp.x, y = comp.y)) + geom_point(size = I(2), aes(color = factor(type))) + 
-  #   theme(legend.position = "right", plot.title = element_text(size = 30), legend.text = element_text(size = 22), 
-  #   legend.title = element_text(size = 20), axis.title = element_text(size = 30), legend.background = element_rect(), 
-  #   axis.text.x = element_text(margin = margin(b = -2)), axis.text.y = element_text(margin = margin(l = -14))) + 
-  #   labs(title = title) + theme_bw() + 
-  #   if (labels == TRUE) {
-  #     geom_text(data = variates.X, mapping = aes(label =(rownames(variates.X))), check_overlap = TRUE, size = 3)
-  #   }
-  # pcx.y
+  variates.X = cbind(Score = rownames(pls.fit$variates$X), 
+                     x.variates)
+  loadings.X = cbind(Loading = rownames(pls.fit$loadings$X), 
+                     x.loadings)
+  rownames(x.exp_variance) = paste0("comp.", seq(1, nrow(x.exp_variance)))
+  write.table(as.data.frame(variates.X), paste0(gsub(".txt", 
+                                                     "", file), "_PLSR_Xscores.txt"), sep = "\t", row.names = F, 
+              quote = F)
+  write.table(as.data.frame(loadings.X), paste0(gsub(".txt", 
+                                                     "", file), "_PLSR_Xloadings.txt"), sep = "\t", row.names = F, 
+              quote = F)
+  write.table(as.data.frame(x.exp_variance), paste0(gsub(".txt", 
+                                                         "", file), "_PLSR_Xpve.txt"), sep = "\t", row.names = T, 
+              quote = F)
 }
