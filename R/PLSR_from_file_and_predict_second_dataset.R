9e4e0c08cd32755937f8e26584f7d9e4c260e43f
graeberlab <nbalanis@mednet.ucla.edu>
2017-12-11 16:33:46 -0800

    Update PLSR_from_file_and_predict_second_dataset.R
    add TCGA option to remove normal samples

--- R/PLSR_from_file_and_predict_second_dataset.R
+++ R/PLSR_from_file_and_predict_second_dataset.R
@@ -30,17 +30,19 @@
 #' @param varimax If T performs Varimax rotation, 
 #' @param varimax.comp # of varimax components, kind of hacky, keep this # the same as # of comps. Will fix later.
 #' @param output_folder the folder to output to, default is ./ i.e. current folder
+#' @param TCGA predicted files are from TCGA, barcodes separated by periods, so remove normal samples, default is FALSE
 #'
 #' @importFrom mixOmics pls
 #' @export
 #'
-PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, sample.type, y.response, 
-                                                     sample.names2 = NULL, sample.type2 = NULL, train_string, test_string, 
-                                                     title = "PLSR", comp.x = "comp.1", comp.y = "comp.2", comps = 2, 
-                                                     labels = F,saveplot=T,
-                                                     savetype = ".png", w = 8, h = 6, legendname = "default",scale=F,
-                                                     plot_both=T,colpalette=NULL,shape.palette=NULL,ellipses=T,conf=0.9,
-                                                     varimax=F,varimax.comp=2,output_folder="./") {
+PLSR_from_file_and_predict_second_dataset=function (file, file2, sample.names, sample.type, y.response, 
+          sample.names2 = NULL, sample.type2 = NULL, train_string, 
+          test_string, title = "PLSR", comp.x = "comp.1", comp.y = "comp.2", 
+          comps = 2, labels = F, saveplot = T, savetype = ".png", w = 8, 
+          h = 6, legendname = "default", scale = F, plot_both = T, 
+          colpalette = NULL, shape.palette = NULL, ellipses = T, conf = 0.9, 
+          varimax = F, varimax.comp = 2, output_folder = "./",TCGA=F) 
+{
   require(mixOmics)
   data = read.table(file, sep = "\t", header = T, stringsAsFactors = FALSE, 
                     quote = "")
@@ -48,6 +50,12 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
               ]
   data2 = read.table(file2, sep = "\t", header = T, stringsAsFactors = FALSE, 
                      quote = "")
+  if(TCGA == T){
+  temp_name=colnames(data2)[1]
+  cancer_samples=which(as.numeric(sapply(colnames(data2)[-1],function(x) strsplit(x,"\\.")[[1]][4])) <= 9) 
+  data2=cbind(data2[,1],data2[,-1][,cancer_samples])
+  colnames(data2)[1]=temp_name
+  }
   data = data[!duplicated(data[, 1]), ]
   data2 = data2[!duplicated(data2[, 1]), ]
   common.genes = intersect_all(data[, 1], data2[, 1])
@@ -64,29 +72,15 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
                 ncomp = comps)
   x.variates = data.frame(pls.fit$variates$X)
   x.loadings = data.frame(pls.fit$loadings$X)
-  
-  if(varimax==T){
-    
-    rotation = varimax(as.matrix(x.loadings[, c(1:(varimax.comp))]), normalize = F) 
-    #rot.load = as.data.frame(matrix(unlist(rotation$loadings), nrow=nrow(x.loadings), byrow=T)) 
-    #rot.load = cbind(Loading = x.loadings[,1], rot.load)
-    
-    
-    scores <- as.matrix(x.variates[,1:(varimax.comp)]) %*% rotation$rotmat
+  if (varimax == T) {
+    rotation = varimax(as.matrix(x.loadings[, c(1:(varimax.comp))]), 
+                       normalize = F)
+    scores <- as.matrix(x.variates[, 1:(varimax.comp)]) %*% 
+      rotation$rotmat
     scores = as.data.frame(scores)
-    colnames(scores)=colnames(x.variates)
-    x.variates=scores
-    
-    #scores = cbind(Score = rownames(x.variates), scores)
-    #write.table(scores, paste0(gsub(".txt", "", file.scores), "_VARIMAX.txt"), row.names = F, sep = "\t", quote = F)
-    
-    
+    colnames(scores) = colnames(x.variates)
+    x.variates = scores
   }
-  
-  
-  
-  
-  
   x.variates$type = sample.type[match(rownames(x.variates), 
                                       sample.names)]
   pc.pred = ggplot(data = x.variates, aes_string(x = comp.x, 
@@ -105,19 +99,17 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
   t.data2 = data.frame(t(data2[, -1]))
   test.predict <- predict(pls.fit, t.data2)
   prediction <- as.data.frame(test.predict$variates)
-  
   colnames(prediction) <- colnames(x.variates)[-ncol(x.variates)]
-  
-  
-  if(varimax==T){
-    predit <- as.matrix(prediction[,1:(varimax.comp)]) %*% rotation$rotmat
-    colnames(predit)=colnames(prediction)
-    prediction=as.data.frame(predit)
-    
+  if (varimax == T) {
+    predit <- as.matrix(prediction[, 1:(varimax.comp)]) %*% 
+      rotation$rotmat
+    colnames(predit) = colnames(prediction)
+    prediction = as.data.frame(predit)
   }
   write.table(cbind(Sample = rownames(prediction), (prediction)), 
-              paste0(output_folder,test_string,"_projected_onto_",train_string, "_PLSR_predicted.scores.txt"), 
-              sep = "\t", row.names = F, quote = F)
+              paste0(output_folder, test_string, "_projected_onto_", 
+                     train_string, "_PLSR_predicted.scores.txt"), sep = "\t", 
+              row.names = F, quote = F)
   prediction$type = sample.type2[match(rownames(prediction), 
                                        sample.names2)]
   pc.pred <- ggplot(prediction, aes_string(x = comp.x, y = comp.y)) + 
@@ -133,37 +125,40 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
                 check_overlap = TRUE, size = 2.3)
     }
   pc.pred
-  if(plot_both==T ){
-    comb=rbind(prediction,x.variates)
+  if (plot_both == T) {
+    comb = rbind(prediction, x.variates)
     pc.pred3 = ggplot(data = comb, aes_string(x = comp.x, 
-                                              y = comp.y),) + geom_point(size = I(3), aes(color = factor(type),shape=factor(type))) + 
-      theme(legend.position = "right", plot.title = element_text(size = 30), 
-            legend.text = element_text(size = 22), legend.title = element_text(size = 20), 
-            axis.title = element_text(size = 30), legend.background = element_rect(), 
-            axis.text.x = element_text(margin = margin(b = -2)), 
-            axis.text.y = element_text(margin = margin(l = -14))) + 
-      labs(title = title) + theme_bw() +
-      if (labels == TRUE) {
+                                              y = comp.y), ) + geom_point(size = I(3), aes(color = factor(type), 
+                                                                                           shape = factor(type))) + theme(legend.position = "right", 
+                                                                                                                          plot.title = element_text(size = 30), legend.text = element_text(size = 22), 
+                                                                                                                          legend.title = element_text(size = 20), axis.title = element_text(size = 30), 
+                                                                                                                          legend.background = element_rect(), axis.text.x = element_text(margin = margin(b = -2)), 
+                                                                                                                          axis.text.y = element_text(margin = margin(l = -14))) + 
+      labs(title = title) + theme_bw() + if (labels == 
+                                             TRUE) {
         geom_text(data = comb, mapping = aes(label = (rownames(comb))), 
                   check_overlap = TRUE, size = 2.5)
       }
-    if(!is.null(shape.palette)){
-      pc.pred3<- pc.pred3 + scale_shape_manual(legendname,values=shape.palette)
+    if (!is.null(shape.palette)) {
+      pc.pred3 <- pc.pred3 + scale_shape_manual(legendname, 
+                                                values = shape.palette)
     }
-    if(!is.null(colpalette)){
-      pc.pred3<- pc.pred3 + scale_color_manual(legendname,values=colpalette)
+    if (!is.null(colpalette)) {
+      pc.pred3 <- pc.pred3 + scale_color_manual(legendname, 
+                                                values = colpalette)
     }
-    if(ellipses==T){
-      pc.pred3<- pc.pred3 + stat_ellipse(aes(color=factor(type)),level=conf)
+    if (ellipses == T) {
+      pc.pred3 <- pc.pred3 + stat_ellipse(aes(color = factor(type)), 
+                                          level = conf)
     }
-    
-    if(saveplot==T){
-      ggsave(paste0(output_folder,test_string,"_projected_onto_",train_string, "_", comp.x, "_vs_", comp.y, savetype), 
+    if (saveplot == T) {
+      ggsave(paste0(output_folder, test_string, "_projected_onto_", 
+                    train_string, "_", comp.x, "_vs_", comp.y, savetype), 
              dpi = 300, plot = pc.pred3, width = w, height = h)
     }
     pc.pred3
-  } else{
-    
+  }
+  else {
     pc.pred = pc.pred + geom_point(data = x.variates, aes_string(x = comp.x, 
                                                                  y = comp.y)) + geom_point(size = I(1.3), aes(color = factor(type))) + 
       theme(legend.position = "right", plot.title = element_text(size = 30), 
@@ -172,11 +167,11 @@ PLSR_from_file_and_predict_second_dataset<-function (file, file2, sample.names, 
             axis.text.x = element_text(margin = margin(b = -2)), 
             axis.text.y = element_text(margin = margin(l = -14))) + 
       labs(title = title) + theme_bw()
-    if(saveplot==T){
-      ggsave(paste0(output_folder,test_string,"_projected_onto_",train_string,"_", comp.x, "_vs_", comp.y, savetype), 
+    if (saveplot == T) {
+      ggsave(paste0(output_folder, test_string, "_projected_onto_", 
+                    train_string, "_", comp.x, "_vs_", comp.y, savetype), 
              dpi = 300, plot = pc.pred, width = w, height = h)
     }
     pc.pred
   }
 }
-
