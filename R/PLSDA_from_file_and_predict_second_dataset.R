01e88f9a29ecc83c6b41b5a247d5a258f773c8fe
Balanis <NBalanis@mednet.ucla.edu>
2017-12-14 21:42:11 -0800

    adding a function that uses plsr to predict everything in a folder

--- R/PLSDA_from_file_and_predict_second_dataset.R
+++ R/PLSDA_from_file_and_predict_second_dataset.R
@@ -23,7 +23,7 @@
 
 
 PLSDA_from_file_and_predict_second_dataset = function(file, file2, sample.names, response.values, sample.names2=NULL,
-                                                      response.values2=NULL, comps = 3, scale = F, ind.names = F,output_folder="./",train_string="",test_string=""){
+         response.values2=NULL, comps = 3, scale = F, ind.names = F,output_folder="./",train_string="",test_string=""){
   require(mixOmics)
   data = read.table(file, sep='\t',header=T,stringsAsFactors=FALSE, quote = "")
   data = data[rowSums((data[, -1] == 0)) < ncol(data[-1]), ] #remove genes with no variance
@@ -50,10 +50,12 @@ PLSDA_from_file_and_predict_second_dataset = function(file, file2, sample.names,
   t.data2 = data.frame(t(data2[,-1])) 
   
   test.predict <- predict(pls.fit, t.data2, method = "max.dist")
-  write.table(test.predict$variates,paste0(output_folder,train_string,"_projected_onto_",test_string,"_prediction_PLSDA_XScores.txt"),col.names=NA,quote=F,sep="\t",row.names=T)
+  write.table(test.predict$variates,paste0(output_folder,train_string,"_projected_onto_",test_string,"_",comps,"_comps_PLSDA_Xscores.txt"),col.names=NA,quote=F,sep="\t",row.names=T)
   
   prediction <- as.data.frame(test.predict$class$max.dist[, comps])
   colnames(prediction)[1] <-"prediction"
+  write.table(prediction,paste0(output_folder,train_string,"_projected_onto_",test_string,"_",comps,"_comps_PLSDA_prediction.txt"),col.names=NA,quote=F,sep="\t",row.names=T)
+
   if (!is.null(sample.names2)) {
     prediction$actual = (response.values2[match(rownames(prediction), sample.names2)])
     print(table(prediction$prediction, droplevels(prediction$actual)))
